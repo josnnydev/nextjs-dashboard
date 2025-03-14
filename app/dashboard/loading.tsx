a8ff0329f00bfa29b8eba345e4a7e16a86427563
josnnydev <pjosnny@gmail.com>
2025-03-13 21:40:08 -0400

    updates

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <p className="text-gray-500">loading...</p>
+  )
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div className='flex items-center justify-center py-6 px-6 md:px-20 lg:px-32 xl:px-40  bg-blue-500 '>Footer</div>
+    <div className='flex items-center justify-center py-6 px-6 md:px-20 lg:px-32 xl:px-40  bg-blue-500 '> 
+    <Link href="/dashboard">Dashboard</Link>
+    </div>
   )
 }
